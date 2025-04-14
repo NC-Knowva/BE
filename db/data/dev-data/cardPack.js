d11970bc23940bfa8ebabb4f4041258e6f8a83e9
bell <bellelm@Belkises-MacBook-Air.local>
2025-04-14 17:55:59 +0100

    added test and dev data for education, users and cardpack, still a few more to go

---
+++ db/data/dev-data/cardPack.js
@@ -0,0 +1,268 @@
+module.exports = [
+    {
+        name: "Maths revision",
+        username: "Knowva",
+        topic: "Maths",
+        education: "GCSE",
+        visibility: true,
+        description: "includes common gcse questions: fractions, algebra, decimals.",
+        questions: [
+            {
+                "Q1": "Write 1/9 as a recurring decimal.",
+                "A1": " 0.1"
+            },
+            {
+                "Q2": "Write 0.3 as a fraction.",
+                "A2": " 1/3"
+            },
+            {
+                "Q3": "Write 0.390 as a fraction.",
+                "A3": "130/333"
+            }
+        ]
+    },
+    {
+        name: "Bio revision",
+        username: "Knowva",
+        topic: "Science",
+        education: "GCSE",
+        visibility: true,
+        description: "includes common biology questions.",
+        questions: [
+            {
+                "Q1": "What is the central nervous system (CNS) made up of?",
+                "A1": "The brain and the spinal cord"
+            },
+            {
+                "Q2": "In genetic engineering, useful genes from one organism can be cut out and...",
+                "A2": "... transferred to the cells of other organisms."
+            },
+            {
+                "Q3": "What is a cell called when it has differentiated?",
+                "A3": "A specialised cell."
+            }
+        ]
+    },
+    {
+        name: "History revision",
+        username: "Knowva",
+        topic: "Medical Renaissance",
+        education: "GCSE",
+        visibility: true,
+        description: "tests your History knowledge.",
+        questions: [
+            {
+                "Q1": "Define the Renaissance",
+                "A1": "Renaissance is a French term that means 'rebirth'. This describes a period of new ideas about science, religion, art and architecture from the 16th century to the 18th century."
+            },
+            {
+                "Q2": "What did Thomas Sydenham believe about patients symptoms?",
+                "A2": "Thomas Sydenham believed that a patient's symptoms indicated the disease they had and that they should all receive the same remedy."
+            },
+            {
+                "Q3": "Define a decree.",
+                "A3": "A decree is an official order made by a monarch or the government. It is illegal not to follow a decree."
+            }
+        ]
+    },
+    {
+        name: "Physics revision",
+        username: "Knowva",
+        topic: "science revision",
+        education: "GCSE",
+        visibility: true,
+        description: "included common physics questions.",
+        questions: [
+            {
+                "Q1": "What am I describing? The displacement from the rest position to the crest or trough of a wave.",
+                "A1": "Amplitude"
+            },
+            {
+                "Q2": "An object is irradiated. What does this mean?",
+                "A2": "The object is exposed to radiation."
+            },
+            {
+                "Q3": "A resultant force of 20 N is applied to a ball resting on a table. The ball would:",
+                "A3": "Start moving in the direction of the force."
+            }
+        ]
+    },
+    {
+        name: "Psychology revision",
+        username: "Knowva",
+        topic: "Resistance to Social Influence",
+        education: "A-level",
+        visibility: true,
+        description: "tests your knowledge with common questions.",
+        questions: [
+            {
+                "Q1": "Describe the role of social support in resistance to social influence.",
+                "A1": "Social support can help individuals resist pressures to conform or obey. If they have support from a dissenter (someone who disagrees with the majority or refuses to obey), it frees the individual from the pressure to conform or obey, allowing them to act independently. This was demonstrated in the Asch study investigating the unanimity of the majority, and in Milgram’s variation where a disobedient confederate refused to obey at a certain point."
+            },
+            {
+                "Q2": "What did Allen and Levine (1971) find about the role of dissenters in conformity?",
+                "A2": "Allen and Levine (1971) found that introducing a dissenter in an Asch-style study greatly reduced conformity levels, even if the dissenter wore thick glasses and claimed they had poor eyesight. This supports the idea that having social support increases resistance to social influence."
+            },
+            {
+                "Q3": "Define the concept of 'Minority Influence'.",
+                "A3": "Minority influence is when a small group (or individual) influences the behaviour and beliefs of a larger group of people. This usually has a smaller effect than conformity to a majority, but an effect is possible."
+            }
+        ]
+    },
+    {
+        name: "Maths revision",
+        username: "Knowva",
+        topic: "Normal Distribution and Missing Parameters",
+        education: "A-level",
+        visibility: true,
+        description: "test your knowledge!",
+        questions: [
+            {
+                "Q1": "What are the characteristics of the normally distributed curve?",
+                "A1": "Symmetrical Continuous Mode= median= mean Total area under curve = 1"
+            },
+            {
+                "Q2": "What is the formula for standardising X results?",
+                "A2": "Where Z has a mean of zero and standard deviation of 1"
+            },
+            {
+                "Q3": "What percentage of the data is within 1 standard deviation, 2 and 3?",
+                "A3": "68%, 95%, 99.7%"
+            }
+        ]
+    },
+    {
+        name: "Sociology revision",
+        username: "Knowva",
+        topic: "Families and Households",
+        education: "A-level",
+        visibility: true,
+        description: "test your knowledge!",
+        questions: [
+            {
+                "Q1": "define New Right:",
+                "A1": "Family is central in social policies. Poor behaviour is down to poor family background. Nuclear family is ideal. Single motherhood leads to a culture of welfare dependency."
+            },
+            {
+                "Q2": "define Postmodernism:",
+                "A2": "Free to construct own identity. Society is a collection of subcultures. Family is less stable. Greater diversity. Domestic violence undermines positives."
+            },
+            {
+                "Q3": "define Policy: Evaluation.",
+                "A3": "Tax and welfare favour heterosexual married couples. Mothers are more often granted custody after a divorce. Housing is designed with the nuclear family in mind. State intervention has increase. Functional single parent may be more beneficial than dysfunctional nuclear family."
+            }
+        ]
+    },
+    {
+        name: "Law revision",
+        username: "Knowva",
+        topic: "Federal Judicial System",
+        education: "A-level",
+        visibility: true,
+        description: "test your knowledge!",
+        questions: [
+            {
+                "Q1": "The Supreme Court has what two types of jurisdiction?",
+                "A1": "Original jurisdiction; and Appellate jurisdiction"
+            },
+            {
+                "Q2": "Can Congress modify the Supreme Court’s original jurisdiction?",
+                "A2": "No, but it can grant concurrent original jurisdiction to the lower federal courts"
+            },
+            {
+                "Q3": "No, but it can grant concurrent original jurisdiction to the lower federal courts",
+                "A3": "SCOTUS will review a state court’s decision if the state law tracks federal law and the state court has a practice of following the federal interpretations of the particular law"
+            }
+        ]
+    },
+    {
+        name: "Sociology catchup",
+        username: "jodie_H",
+        topic: "Crime and deviance",
+        education: "A-level",
+        visibility: true,
+        description: "secure your A grades!",
+        questions: [
+            {
+                "Q1": "Marxism: Snider.",
+                "A1": "Laws threatening corporations are rarely passed or not enforced. Corporate crimes are more harmful than street crimes but less prosecuted. Easier to prosecute individuals."
+            },
+            {
+                "Q2": "Marxism: Taylor, Walton and Young.",
+                "A2": "Should consider wider aspects of crime. Crime is a conscious and deliberate choice."
+            },
+            {
+                "Q3": "Interactionist: Cicourel.",
+                "A3": "Typification of a criminal: poor, single parent, working class, ethnic minority. Middle class parents can negotiate children out of trouble."
+            }
+        ]
+    },
+    {
+        name: "English literature A-level help",
+        username: "your_fav_ai",
+        topic: "The Great Gatsby",
+        education: "A-level",
+        visibility: true,
+        description: "analysis revision",
+        questions: [
+            {
+                "Q1": "What is the quotation where Nick is recalling his father’s advice?",
+                "A1": "“Just remember that all the people in the world haven’t had the advantages you’ve had”"
+            },
+            {
+                "Q2": "What are 3 ways you can analyse Nick’s father’s advice?.",
+                "A2": "Nick states he doesn’t judge “Advantages” relates to wealth and social status of his family. Nick suggests other wealthy people are snobby"
+            },
+            {
+                "Q3": "How were new money described by old money?",
+                "A3": "New money lacked social refinement"
+            }
+        ]
+    },
+    {
+        name: "Maths GCSE revision",
+        username: "boss_bernard",
+        topic: "equations and values",
+        education: "GCSE",
+        visibility: true,
+        description: "gain the math boss title",
+        questions: [
+            {
+                "Q1": "sin(45)",
+                "A1": "√2/2 or 1/√2"
+            },
+            {
+                "Q2": "tan(45)",
+                "A2": "1"
+            },
+            {
+                "Q3": "cos(30)",
+                "A3": "√3/2"
+            }
+        ]
+    },
+
+    {
+        name: "(Biology) science revision",
+        username: "grades-smasher",
+        topic: "Biochemical Reactions in Cells are Controlled by Enzymes ",
+        education: "A-level",
+        visibility: true,
+        description: "master biology",
+        questions: [
+            {
+                "Q1": "How is a substrate molecule held within an active site?",
+                "A1": "By bonds that form temporarily between amino acids of active site and groups on substrate molecules"
+            },
+            {
+                "Q2": "Describe the ‘lock and key’ model",
+                "A2": "Where substrate fits into enzyme in the same way a key fits into a lock"
+            },
+            {
+                "Q3": "If the substrate concentration decreases with time then the rate of reaction will…",
+                "A3": "decrease over time"
+            }
+        ]
+    },
+
+]
---
+++ db/data/test-data/education.js
@@ -0,0 +1,8 @@
+module.exports=[
+    {
+        education_id:"A-level"
+    },
+    {
+        education_id:"GCSE"
+    }
+]
