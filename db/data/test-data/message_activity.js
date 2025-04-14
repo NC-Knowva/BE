4ad02cd9313298d619f4e9c1181bd41607a0baac
Unamta Aslam <aslamunamta@gmail.com>
2025-04-14 11:56:30 +0100

    Add users and message_activity test data

---
+++ db/data/test-data/message_activity.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    id: 1,
+    sender_username: "phuyton0",
+    receiver_username: "dfarington1",
+    body: "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
+  },
+  {
+    id: 2,
+    sender_username: "phuyton0",
+    receiver_username: "gcass1",
+    body: "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+  },
+  {
+    id: 3,
+    sender_username: "phuyton0",
+    receiver_username: "wpittman2",
+    body: "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+  },
+  {
+    id: 4,
+    sender_username: "phuyton0",
+    receiver_username: "wgiorgini3",
+    body: "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+  },
+  {
+    id: 5,
+    sender_username: "dfarington1",
+    receiver_username: "wpittman2",
+    body: "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+  },
+  {
+    id: 6,
+    sender_username: "dfarington1",
+    receiver_username: "wgiorgini3",
+    body: "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+  },
+  {
+    id: 7,
+    sender_username: "wpittman2",
+    receiver_username: "phuyton0",
+    body: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+  },
+  {
+    id: 8,
+    sender_username: "wpittman2",
+    receiver_username: "phuyton0",
+    body: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
+  },
+  {
+    id: 9,
+    sender_username: "wpittman2",
+    receiver_username: "wgiorgini3",
+    body: "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+  },
+  {
+    id: 10,
+    sender_username: "wpittman2",
+    receiver_username: "wgiorgini3",
+    body: "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.",
+  },
+];
---
+++ db/data/test-data/users.js
@@ -0,0 +1,102 @@
+module.exports = [
+  {
+    username: "phuyton0",
+    name: "Penni Huyton",
+    password:
+      "24eb8086b86f381ce27543224550f650351d9aaef04994fcb64a75110d76253f",
+    email: "phuyton0@ocn.ne.jp",
+    avatar_img_url: "http://dummyimage.com/126x100.png/ff4444/ffffff",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "dfarington1",
+    name: "Dixie Farington",
+    password:
+      "108b125f169f892bcbc9eef413aba571ec21159b91cc115f2f4ebc0a974ffb30",
+    email: "dfarington1@dailymail.co.uk",
+    avatar_img_url: "http://dummyimage.com/196x100.png/cc0000/ffffff",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "wpittman2",
+    name: "Willy Pittman",
+    password:
+      "66b8e652e6f91b6710fc58fe9e3cfa55f1c23ea8b42ac8df852d1706fefba4b6",
+    email: "wpittman2@independent.co.uk",
+    avatar_img_url: "http://dummyimage.com/249x100.png/cc0000/ffffff",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "wgiorgini3",
+    name: "Welby Giorgini",
+    password:
+      "097a4b80d27e1ea2d4b547f71c8139b9fe60de02d7b39db3f6d59d5cbf967f86",
+    email: "wgiorgini3@mozilla.org",
+    avatar_img_url: "http://dummyimage.com/169x100.png/ff4444/ffffff",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "prubinowitch4",
+    name: "Pennie Rubinowitch",
+    password:
+      "09fa044800d400a30fd761ba8d078c1b221221d355ba4683bfc2e8012a318d35",
+    email: "prubinowitch4@time.com",
+    avatar_img_url: "http://dummyimage.com/160x100.png/5fa2dd/ffffff",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "rseamons5",
+    name: "Rudy Seamons",
+    password:
+      "5467c060e9cb65715f9633ece45508b46c61fcf04cc1cff53f4386485529478a",
+    email: "rseamons5@jimdo.com",
+    avatar_img_url: "http://dummyimage.com/213x100.png/dddddd/000000",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "dgurnay6",
+    name: "Dene Gurnay",
+    password:
+      "67f9d760f71376deb1a5fbb5f996a6a950d665d4d4fc78e93aaf7e6c6ce75001",
+    email: "dgurnay6@ted.com",
+    avatar_img_url: "http://dummyimage.com/155x100.png/dddddd/000000",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "sguido7",
+    name: "Shelli Guido",
+    password:
+      "4b3f98fc245dad1a4a103d2f61f53024edeb603c6ed276e34f2d81a23475d772",
+    email: "sguido7@bravesites.com",
+    avatar_img_url: "http://dummyimage.com/240x100.png/ff4444/ffffff",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "cbyles8",
+    name: "Case Byles",
+    password:
+      "42497f7b8ec18be41f07c942d7245bf6460fbc3b881fff5e1189f9061e5517b2",
+    email: "cbyles8@cdbaby.com",
+    avatar_img_url: "http://dummyimage.com/216x100.png/dddddd/000000",
+    settings: [],
+    calendar: [],
+  },
+  {
+    username: "krisbrough9",
+    name: "Katrinka Risbrough",
+    password:
+      "6356cf6378058d7f91938e845a616d58ac53258bfa69a45f4bdcd8d7ada50527",
+    email: "krisbrough9@latimes.com",
+    avatar_img_url: "http://dummyimage.com/154x100.png/cc0000/ffffff",
+    settings: [],
+    calendar: [],
+  },
+];
