8b774bee177dbbcd2f6af6a8e0d2ec050d481420
Unamta Aslam <aslamunamta@gmail.com>
2025-04-14 16:57:08 +0100

    Fix test data index.js

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,7 +1,7 @@
 exports.card_pack = require("./card_pack.js");
 exports.games = require("./games.js");
 exports.message_activity = require("./message_activity.js");
-exports.study_group.js = require("./study_group.js");
+exports.study_group = require("./study_group.js");
 exports.topics = require("./topics.js");
 exports.education_level = require("./education_level.js");
 exports.scoreboard = require("./scoreboard.js");
