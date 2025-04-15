0880bb6fcc4485194ab47bc7352f05425377d33d
bell <bellelm@Belkises-MacBook-Air.local>
2025-04-15 16:44:54 +0100

    done with dev data just two more files left to go

---
+++ db/data/dev-data/message_activity.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        sender_username:"kathy2008",
+        receiver_username:"studywithEM",
+        body:"which college are you thinking of applying?"
+
+    },
+    {
+        sender_username:"grades-smasher",
+        receiver_username:"iheartsocio",
+        body:"shall we revise later tonight?"
+
+    },
+    {
+        sender_username:"kathy2008",
+        receiver_username:"boss_bernard",
+        body:"you ready for our exam on monday?"
+
+    },
+    {
+        sender_username:"study_basher",
+        receiver_username:"grades-smasher",
+        body:"test me on my science flashcards plz!"
+
+    },
+    {
+        sender_username:"iheartsocio",
+        receiver_username:"grades-smasher",
+        body:"yeah i'm up for revision tonight."
+
+    },
+    {
+        sender_username:"your_fav_ai",
+        receiver_username:"jodie_H",
+        body:"i've aced all the mock exams easy"
+
+    },
+    {
+        sender_username:"studywithEM",
+        receiver_username:"boss_bernard",
+        body:"do you know any useful communities i can join for maths help?"
+
+    }
+]
