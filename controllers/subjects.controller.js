a2f7fbf35d0cdecc3e172619181a3eeb0b1db5c0
Serhii Derkach <serhiiderkach@gmail.com>
2025-04-22 14:21:07 +0100

    get subjects completed

---
+++ controllers/subjects.controller.js
@@ -0,0 +1,7 @@
+const { selectSubjects } = require("../models/subjects.model");
+
+exports.getSubjects = (req, res, next) => {
+    selectSubjects()
+    .then(subjects => res.status(200).send({ subjects }))
+    .catch(err => next(err));
+};
