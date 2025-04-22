8e73c8044e19b6aec630da86f6a93e554e233166
Unamta Aslam <aslamunamta@gmail.com>
2025-04-22 23:15:06 +0100

    add a few endpoints to endpoints.json

--- app.js
+++ app.js
@@ -2,22 +2,33 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+const { getEndpoints } = require("./controllers/api.controller");
+
 const { getGames } = require("./controllers/games.controller");
 
-
-const { getUsers, getUserByUsername, getMessagesByUsername, getStudyGroupsByUsername } = require("./controllers/users.controller");
-const { invalidPathController, psqlErrorHandler, customErrorHandler, serverErrorHandler } = require("./controllers/errors.controller");
+const {
+  getUsers,
+  getUserByUsername,
+  getMessagesByUsername,
+  getStudyGroupsByUsername,
+} = require("./controllers/users.controller");
+const {
+  invalidPathController,
+  psqlErrorHandler,
+  customErrorHandler,
+  serverErrorHandler,
+} = require("./controllers/errors.controller");
 const { getSubjects } = require("./controllers/subjects.controller");
-
 
 app.use(cors());
 
 app.use(express.json());
 
+app.get("/api", getEndpoints);
+
 app.get("/api/users", getUsers);
 
 app.get("/api/users/:username", getUserByUsername);
-
 
 app.get("/api/games", getGames);
 
@@ -26,7 +37,6 @@ app.get("/api/users/:username/messages", getMessagesByUsername);
 app.get("/api/users/:username/study_groups", getStudyGroupsByUsername);
 
 app.get("/api/subjects", getSubjects);
-
 
 app.use(invalidPathController);
 
@@ -37,4 +47,3 @@ app.use(customErrorHandler);
 app.use(serverErrorHandler);
 
 module.exports = app;
-
