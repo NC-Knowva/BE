0dc90a262fd01e0813626c5b68e3a0661eaf9293
Serhii Derkach <serhiiderkach@gmail.com>
2025-04-23 14:46:12 +0100

    get /cards/:name completed

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const {
 } = require("./controllers/errors.controller");
 const { getSubjects } = require("./controllers/subjects.controller");
 const { getTopics, postTopic } = require("./controllers/topics.controller");
-const { getCards } = require("./controllers/cards.controller");
+const { getCards, getCardByName } = require("./controllers/cards.controller");
 const { getScoreboard } = require("./controllers/scoreboard.controller");
 
 app.use(cors());
@@ -53,6 +53,8 @@ app.get("/api/topics", getTopics);
 app.post("/api/topics", postTopic);
 
 app.get("/api/cards", getCards);
+
+app.get("/api/cards/:name", getCardByName);
 
 app.get("/api/scoreboard", getScoreboard);
 
