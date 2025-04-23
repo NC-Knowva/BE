a4127aec133e476440e0e4eeec032cebc7276916
bell <bellelm@Belkises-MacBook-Air.local>
2025-04-23 16:33:28 +0100

    subject by id

--- controllers/subjects.controller.js
+++ controllers/subjects.controller.js
@@ -1,7 +1,18 @@
-const { selectSubjects } = require("../models/subjects.model");
+const { selectSubjects, fetchSubjectById } = require("../models/subjects.model");
 
 exports.getSubjects = (req, res, next) => {
     selectSubjects()
-    .then(subjects => res.status(200).send({ subjects }))
-    .catch(err => next(err));
+        .then(subjects => res.status(200).send({ subjects }))
+        .catch(err => next(err));
 };
+
+exports.getSubjectById = (req, res, next) => {
+    const { subject_id } = req.params
+    fetchSubjectById(subject_id)
+        .then((subject) => {
+            res.status(200).send({ subject })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
