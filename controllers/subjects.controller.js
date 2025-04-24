0193ae9eec9806c87590a35e3063d4a1e4f8ce0a
Unamta Aslam <aslamunamta@gmail.com>
2025-04-24 01:53:01 +0100

    subject filter finished

--- controllers/subjects.controller.js
+++ controllers/subjects.controller.js
@@ -2,8 +2,7 @@ const { selectSubjects } = require("../models/subjects.model");
 
 exports.getSubjects = (req, res, next) => {
   let { filter_by } = req.query;
-  selectSubjects()
+  selectSubjects(filter_by)
     .then((subjects) => res.status(200).send({ subjects }))
     .catch((err) => next(err));
 };
-
--- models/subjects.model.js
+++ models/subjects.model.js
@@ -1,6 +1,15 @@
 const db = require("../db/connection");
 
-exports.selectSubjects = () => {
-    const queryStr = "SELECT * FROM subjects";
-    return db.query(queryStr).then(({ rows }) => rows);
+exports.selectSubjects = (filter_by) => {
+  const queryStr = "SELECT * FROM subjects";
+  return db.query(queryStr).then(({ rows }) => {
+    if (filter_by === undefined) {
+      return rows;
+    } else {
+      return rows.filter((elem) => {
+        return elem.education_id === filter_by;
+      });
+    }
+  });
 };
+
